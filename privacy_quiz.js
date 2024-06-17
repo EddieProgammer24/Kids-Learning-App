4a4159327a531f21ba612a5e2b3312222d6e14b4
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-17 22:18:28 +0300

    Privacy Section

---
+++ privacy_quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Why shouldn’t you share your passwords with friends?",
+        answer: "B. They might use your account without permission",
+        options: [
+            "A. They might forget them",
+            "B. They might use your account without permission",
+            "C. They might change your password",
+            "D. They might write it down"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What should you do if you receive a message from someone you do not know?",
+        answer: "B. Your email address",
+        options: [
+            "A. Your favorite color",
+            "B. Your email address",
+            "C. Your favorite food",
+            "D. Your school project"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What should you do if a website asks for your personal information?",
+        answer: "C.Ask a trusted adult for advice",
+        options: [
+            "A. Give it to them right away",
+            "B. Ignore the request",
+            "C. Ask a trusted adult for advice",
+            "D. Enter false information"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is a safe way to use social media?",
+        answer: "B. Using a private account and only accepting friends you know",
+        options: [
+            "A. Sharing your home address in posts",
+            "B. Using a private account and only accepting friends you know",
+            "C. Posting all your daily activities",
+            "D. Adding everyone who sends a friend request"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How often should you change your passwords?",
+        answer: "B. To prevent others from accessing your information",
+        options: [
+            "A. To save battery",
+            "B. To prevent others from accessing your information",
+            "C. To avoid slow performance",
+            "D. To clear your browsing history"
+        ]
+    }
+];
