303f27967ab325fe014b17ae64f8951bdea54d58
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-17 20:46:45 +0300

    Final Commit

---
+++ cyber_quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What should you do if you are being bullied online?",
+        answer: "B. Ignore it and tell a trusted adult",
+        options: [
+            "A. Fight back",
+            "B. Ignore it and tell a trusted adult",
+            "C. Share the bully's message",
+            "D. Post about it on social media"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the best way to support a friend who is being cyberbullied?",
+        answer: "C. Listen to them and tell a trusted adult",
+        options: [
+            "A. Join in on the bullying",
+            "B. Encourage them to bully back",
+            "C. Listen to them and tell a trusted adult",
+            "D. Ignore them"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of these is a sign of cyberbullying?",
+        answer: "B. Repeated mean messages",
+        options: [
+            "A. Compliments from friends",
+            "B. Repeated mean messages",
+            "C. Invitations to play games",
+            "D. Friendly chats"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How can you help stop cyberbullying?",
+        answer: "A.Report it to a trusted adult or platform",
+        options: [
+            "A. Report it to a trusted adult or platform",
+            "B. Ignore it",
+            "C. Share it with everyone",
+            "D. Laugh about it"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Why is it important to be kind online?",
+        answer: "C. To create a positive online environment",
+        options: [
+            "A. To make more friends",
+            "B. To avoid being bullied",
+            "C. To create a positive online environment",
+            "D. To win more games"
+        ]
+    }
+];
