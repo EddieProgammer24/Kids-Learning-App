ddd90ffe76ec2ca0ab9a5defc3d35d0cef4b8bde
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-17 12:19:50 +0300

    Score Section

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the best way to create a strong password?",
+        answer: "C. A mix of letters, numbers, and symbols",
+        options: [
+            "A. Your birthdate",
+            "B. Your pet's name",
+            "C. A mix of letters, numbers, and symbols",
+            "D.  The word 'Password'"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What should you do if you receive a message from someone you do not know?",
+        answer: "B. Ignore it and tell a trusted adult",
+        options: [
+            "A. Reply to them",
+            "B. Ignore it and tell a trusted adult",
+            "C. Give them your personal information",
+            "D. Delete the message"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is safe to share online?",
+        answer: "C. Your favorite hobby",
+        options: [
+            "A. Your home address",
+            "B. Your phone number",
+            "C. Your favorite hobby",
+            "D. Your school name"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the first thing you should do if you see something inappropriate online?",
+        answer: "C. Close the page and tell a trusted adult",
+        options: [
+            "A. Keep looking at it",
+            "B. Show it to your friends",
+            "C. Close the page and tell a trusted adult",
+            "D. Save it for later"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How often should you change your passwords?",
+        answer: "D. Every six months",
+        options: [
+            "A. Never",
+            "B. Once a year",
+            "C. Every month",
+            "D. Every six months"
+        ]
+    }
+];
