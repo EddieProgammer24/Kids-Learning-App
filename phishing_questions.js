c495cf828136012d96d136decb3ad76100b0a84c
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-17 23:15:33 +0300

    Phishing Section

---
+++ phishing_questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is a phishing scam?",
+        answer: "B. An attempt to trick you into giving out personal information",
+        options: [
+            "A. A type of game",
+            "B. An attempt to trick you into giving out personal information",
+            "C. A new social media trend",
+            "D. A way to clean your computer"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which email should you be cautious of?",
+        answer: "B. One from an unknown sender asking for your password",
+        options: [
+            "A. One from your best friend",
+            "B. One from an unknown sender asking for your password",
+            "C. One from your school teacher",
+            "D. One from your family member"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What should you do if you receive a suspicious link?",
+        answer: "D.Report it to a trusted adult",
+        options: [
+            "A. Click on it to see where it goes",
+            "B. Delete it immediately",
+            "C. Share it with friends",
+            "D. Report it to a trusted adult"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How can you tell if a website is secure?",
+        answer: "B. It starts with “https” and has a padlock icon",
+        options: [
+            "A. It has a lot of advertisements",
+            "B. It starts with “https” and has a padlock icon",
+            "C. It has colorful graphics",
+            "D. It asks for your personal information right away"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What should you do if you accidentally give out personal information in a phishing scam?",
+        answer: "B. Change your passwords and tell a trusted adult",
+        options: [
+            "A. Ignore it and hope for the best",
+            "B. Change your passwords and tell a trusted adult",
+            "C. Share the incident on social media",
+            "D. Do nothing"
+        ]
+    }
+];
