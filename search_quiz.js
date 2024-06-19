1b0a722e09856b50d0c94a3531f5c794b0e1e51d
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-19 16:19:54 +0300

    Complete

---
+++ search_quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is a safe way to search for information online?",
+        answer: "A. Use kid-friendly search engines",
+        options: [
+            "A. Use kid-friendly search engines",
+            "B. Click on the first link you see",
+            "C. Search for anything without supervision",
+            "D. Use any search engine"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What should you do if you accidentally find inappropriate content?",
+        answer: "B. Close the page and tell a trusted adult",
+        options: [
+            "A. Keep looking at it",
+            "B. Close the page and tell a trusted adult",
+            "C. Share it with friends",
+            "D. Ignore it and move on"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Why is it important to use safe search settings?",
+        answer: "B.  To filter out inappropriate or harmful content",
+        options: [
+            "A. To make searching faster",
+            "B. To filter out inappropriate or harmful content",
+            "C. To save internet data",
+            "D. To find more websites"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is one benefit of using kid-friendly search engines?",
+        answer: "B. They provide safer search results for children",
+        options: [
+            "A. They have more advertisements",
+            "B. They provide safer search results for children",
+            "C. They only show images",
+            "D. They are slower to use"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How can you tell if a website is appropriate for kids?",
+        answer: "D. It is recommended by a trusted adult or educator",
+        options: [
+            "A. It has bright colors",
+            "B. It ends with .com",
+            "C. It is recommended by a trusted adult or educator",
+            "D. It has games"
+        ]
+    }
+];
